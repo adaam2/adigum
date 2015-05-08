4096c775b469e55c38ece7b04a3dbfa49e479ad2
Adam Bull <adamjamesbull@googlemail.com>
2015-05-08 21:52:23 +0100

    First commit

--- header.php
+++ header.php
@@ -5,14 +5,14 @@
     else {
         $isotype = get_stylesheet_directory_uri() . '/assets/img/santiagum-isotype.png';
     }
-    
+
     if(ot_get_option('site_favicon')) {
         $favicon = ot_get_option('site_favicon');
     }
     else {
         $favicon = get_stylesheet_directory_uri() . '/assets/img/santiagum-favicon.png';
     }
-    
+
     if(ot_get_option('site_tw_url')) {
         $twitter_username = parse_url(ot_get_option('site_tw_url'));
         $twitter_username = $twitter_username['path'];
@@ -25,13 +25,13 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="utf-8" />
-        
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="robots" content="index, follow, archive">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        
+
         <title><?php echo w_title('—'); ?></title>
-        
+
         <!-- meta -->
         <meta name="description" content="<?php bloginfo('description'); ?>" />
         <meta name="HandheldFriendly" content="True" />
@@ -46,7 +46,7 @@
         <meta name="twitter:creator" content="<?php echo str_replace('/', '@', $twitter_username); ?>">
         <?php endif; ?>
         <meta name="google-site-verification" content="">
-        
+
         <meta property="og:site_name" content="<?php bloginfo('name'); ?>">
         <meta property="og:type" content="blog">
         <?php if(is_single() OR is_page()) : ?>
@@ -60,55 +60,55 @@
         <?php endif; ?>
         <meta property="og:image" content="<?php echo w_check_thumbnail(); ?>">
         <!-- /meta -->
-        
+
         <!-- styles -->
         <link href="<?php echo $favicon; ?>" rel="shortcut icon">
-        <link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/main.css" rel="stylesheet">
+        <link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/stylesheets/css/main.css" rel="stylesheet">
         <link href="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/styles/github.css" rel="stylesheet">
         <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
         <!-- /styles -->
-        
+
         <!-- wp head -->
         <?php wp_head(); ?>
         <!-- /wp head -->
     </head>
-    
+
     <body <?php body_class(); ?>>
         <!-- aside nav -->
-        <aside>
+        <!-- <aside>
             <h5>Search</h5>
-            
+
             <form role="search" method="get" action="<?php bloginfo('url'); ?>/">
                 <input type="search" class="search-field" placeholder="Search for..." value="" name="s" title="Search for:" />
             </form>
-            
+
             <h5>Pages</h5>
-            
+
             <ul class="list-pages">
                 <li><a href="<?php bloginfo('url'); ?>">Home</li>
                 <?php wp_list_pages(array('title_li' => false)); ?>
             </ul>
-            
+
             <h5>Categories</h5>
-            
+
             <ul class="list-categories">
                 <?php wp_list_categories(array('title_li' => false, 'show_count' => 1)); ?>
             </ul>
-            
+
             <h5>Archives</h5>
-            
+
             <?php
                 $archives = wp_get_archives(array('show_post_count' => 1, 'echo' => 0));
-                $archives = 
+                $archives =
                 $archives = str_replace('</a>&nbsp;(', '</a> <span>', $archives);
                 $archives = str_replace(')', '</span>', $archives);
             ?>
             <ul class="list-archives">
                 <?php echo $archives; ?>
             </ul>
-        </aside>
+        </aside> -->
         <!-- /aside nav -->
-        
+
         <!-- wrapper -->
         <div class="wrapper">
             <a href="<?php bloginfo('url'); ?>" class="logo-santiagum"><span class="logo" style="background-image: url(<?php echo $isotype; ?>);"></span></a>
--- index.php
+++ index.php
@@ -1,51 +1,20 @@
 <?php get_header(); ?>
-            
+
             <?php include('teaser.php'); ?>
-            
+
             <!-- content -->
             <main class="content" role="main">
-                <?php
-                    $sticky = get_option('sticky_posts');
-                    rsort($sticky);
-                    $sticky = array_slice($sticky, 0, 5);
-                    
-                    query_posts(array('post__in' => $sticky));
-                    
-                    if(have_posts()) :
-                ?>
-                <h5 class="index-headline featured"><span>Featured Posts</span></h5>
-                
-                <div class="container featured">
-                    <?php while(have_posts()) : the_post(); ?>
-                    <article <?php post_class(); ?> role="article">
-                        <div class="article-item">
-                            <header class="post-header">
-                                <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                            </header>
-                            
-                            <section class="post-excerpt">
-                                <?php the_excerpt(); ?>
-                            </section>
-                            
-                            <div class="post-meta">
-                                <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F j, Y'); ?></time> on <?php the_category(', ') ?>
-                            </div>
-                        </div>
-                    </article>
-                    <?php endwhile; wp_reset_query(); ?>
-                </div>
-                <?php endif; ?>
-                
+
                 <h5 class="index-headline normal"><span>Latest Posts</span></h5>
-                
+
                 <?php include('loop.php'); ?>
             </main>
             <!-- /content -->
-            
+
             <!-- footer -->
             <footer class="site-footer">
                 <a class="subscribe icon-feed" href="<?php bloginfo('rss2_url'); ?>"><span class="tooltip"> Subscribe!</span></a><br><br>
-                
+
                 <div class="inner">
                     <section class="copyright">All content copyright <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> &copy; <?php echo date('Y'); ?> &bull; Some rights reserved.</section>
                     <section class="poweredby">Proudly powered by <a href="http://wordpress.org" rel="external"> WordPress</a> & <a href="http://juanpablob.com/santiagum/" rel="external">Santiagum Theme</a>.</section>
@@ -54,5 +23,5 @@
             <!-- /footer -->
         </div>
         <!-- /wrapper -->
-            
+
 <?php get_footer();?>
